41b47f1c6c3a087712be670e07abfb22c41e4e9b
nadiiabilockon <11071995nad@gmail.com>
2020-04-25 22:43:31 +0300

    added scroll top

--- js/main.js
+++ js/main.js
@@ -10,3 +10,23 @@ navLinks.forEach(link => {
         document.body.classList.remove('nav-open');
     })
 })
+
+//Get the button
+const scrollButton = document.getElementById("scroller");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () { scrollFunction() };
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        scrollButton.style.display = "block";
+    } else {
+        scrollButton.style.display = "none";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
